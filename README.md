25519c39192fd7ce61a7c9eb38e0dc6360321d68
WoKak <michal.woj34@gmail.com>
2017-12-11 19:44:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# socialapp-frontend
+
+Whole application is in development, more sophisticated readme will be provided sooner or later.
+
+#### To run this app:
+
+* Clone code
+* Install maven
+* mvn spring-boot:run
---
+++ src/main/java/com/pw/socialappbackend/SocialappBackendApplication.java
@@ -0,0 +1,12 @@
+package com.pw.socialappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialappBackendApplication.class, args);
+	}
+}
