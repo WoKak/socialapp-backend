8bfe8eceb330e210ebf7fa333a7f0007e3dfc1c3
WoKak <michal.woj34@gmail.com>
2018-01-07 20:30:42 +0100

    basic connetion beetween frontend and backend

--- src/main/java/com/pw/socialappbackend/web/HelloController.java
+++ src/main/java/com/pw/socialappbackend/web/HelloController.java
@@ -3,12 +3,17 @@ package com.pw.socialappbackend.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
 @RestController
 @RequestMapping("/app")
 public class HelloController {
 
+    @GET
     @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World!";
+    public Response hello() {
+        return Response.ok().build();
     }
 }
