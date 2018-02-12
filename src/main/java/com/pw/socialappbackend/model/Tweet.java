38e1fd008c481ec956cd9e3d348c144b0c2e3fa9
WoKak <michal.woj34@gmail.com>
2018-02-12 19:49:30 +0100

    adding files

--- src/main/java/com/pw/socialappbackend/model/Tweet.java
+++ src/main/java/com/pw/socialappbackend/model/Tweet.java
@@ -3,12 +3,14 @@ package com.pw.socialappbackend.model;
 public class Tweet {
 
     private String tweet;
+    private String owner;
 
     public Tweet() {
     }
 
-    public Tweet(String tweet) {
+    public Tweet(String tweet, String owner) {
         this.tweet = tweet;
+        this.owner = owner;
     }
 
     public String getTweet() {
@@ -17,5 +19,13 @@ public class Tweet {
 
     public void setTweet(String tweet) {
         this.tweet = tweet;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 }
