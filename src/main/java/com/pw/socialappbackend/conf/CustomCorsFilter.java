55c6a1b7e3e538da4a7fa903a72c87cced362638
WoKak <michal.woj34@gmail.com>
2018-03-22 18:56:29 +0100

    changes caused by introducing ssl

--- src/main/java/com/pw/socialappbackend/conf/CustomCorsFilter.java
+++ src/main/java/com/pw/socialappbackend/conf/CustomCorsFilter.java
@@ -22,7 +22,7 @@ public class CustomCorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "https://localhost:4200");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Token, Content-Type");
         response.setHeader("Access-Control-Max-Age", "3600");
